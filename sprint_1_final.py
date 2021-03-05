23063d407bdd2e4825773d976f23df5fa79f658d
DybkoAN <chead.imreoir@gmail.com>
2021-03-05 12:34:15 +0300

    add final task

--- sprint_1_final.py
+++ sprint_1_final.py
@@ -1,10 +1,15 @@
 with open('input.txt', 'r') as reader:
-    len_street = reader.readline()
-    street = [int(i) for i in reader.readline()]
+    len_street = int(reader.readline())
+    street = reader.readline().split()
     diff_to_zero = [0]*len(street)
     i = 0
+    i_zero = None
     while i < len_street:
+        street[i] = int(street[i])
         if street[i] == 0:
+            i_zero = i
             diff_to_zero[i] = 0
+        if i_zero and street[i]!:
+
 
         i += 1
