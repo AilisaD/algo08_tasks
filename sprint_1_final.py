e01309c107c05ed57f2b333c159aea0d1c33f2c8
DybkoAN <chead.imreoir@gmail.com>
2021-03-09 22:56:00 +0300

    remake sprint_1_1

--- sprint_1_final.py
+++ sprint_1_final.py
@@ -3,33 +3,30 @@ import time
 
 
 def task_1():
+    # разбить на функции
     with open('input.txt', 'r') as reader:
         len_street = int(reader.readline())
-        street = reader.readline().split()
-        diff_to_zero = [0]*len_street
-        i = 0
-        i_zero, pre_i_zero = None, None
-        while i < len_street:
-            street[i] = int(street[i])
-            if street[i] == 0:
-                pre_i_zero = i_zero
-                i_zero = i
-                diff_to_zero[i] = 0
-                j = None
-                if pre_i_zero is not None:
-                    j = pre_i_zero + (i_zero - pre_i_zero) // 2 + 1
-                if pre_i_zero is None and i != 0:
-                    j = 0
-                if j is not None:
-                    while j < i_zero:
-                        diff_to_zero[j] = i_zero - j
-                        j += 1
-                    j = None
-            if i_zero is not None and street[i] != 0:
-                diff_to_zero[i] = i - i_zero
-            i += 1
-        with open('output.txt', 'w') as writer:
-            writer.write(f'{" ".join([str(i) for i in diff_to_zero])}')
+        street = reader.readline().split()  # пойти по байтам
+    diff_to_zero = [0]*len_street
+    i_zero, pre_i_zero = None, None
+    for elem in enumerate(street):
+        if elem[1] == '0':
+            pre_i_zero = i_zero
+            i_zero = elem[0]
+            diff_to_zero[i_zero] = 0
+            j = None
+            if pre_i_zero is not None:
+                j = pre_i_zero + (i_zero - pre_i_zero) // 2 + 1
+            if pre_i_zero is None and elem[0] != 0:
+                j = 0
+            if j is not None:
+                while j < i_zero:
+                    diff_to_zero[j] = i_zero - j
+                    j += 1
+        if i_zero is not None and elem[1] != 0:
+            diff_to_zero[elem[0]] = elem[0] - i_zero
+    with open('output.txt', 'w') as writer:
+        writer.write(" ".join([str(i) for i in diff_to_zero]))
 
 
 def task_2():
@@ -40,14 +37,12 @@ def task_2():
             if c.isdigit():
                 table[int(c)-1] += 1
         a = sum([1 for i in table if 0 < i <= possible_num])
-        with open('output.txt', 'w') as writer:
-            writer.write(f'{a}')
-
-
+    with open('output.txt', 'w') as writer:
+        writer.write(f'{a}')
 
 
 time_start = time.time()
-task_2()
+task_1()
 time_finish = time.time()
 time_span = time_finish - time_start
 print(time_span, 'seconds')
