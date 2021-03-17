74e862550bc6c4f32b0f13e24c8e02c2c56f25ba
AilisaD <chead.imreoir@gmail.com>
2021-03-17 11:20:03 +0300

    end sprint_1

--- foreview/sprint_1_1.py
+++ foreview/sprint_1_1.py
@@ -1,34 +1,43 @@
 # 49303069
-def diff_before_i_zero(i, diff_to_zero, i_zero):
+def diff_before_i_zero(num_ref_house, diff_to_zero, i_zero):
+    i = num_ref_house
     while i < i_zero:
         diff_to_zero[i] = str(i_zero - i)
         i += 1
 
 
 def main():
-    with open('input.txt', 'r') as reader:
+    with open('../input.txt', 'r') as reader:
         len_street = int(reader.readline())
         street = reader.readline().split()
+
     diff_to_zero = ['0'] * len_street
     i_zero, pre_i_zero = None, None
-    for elem in enumerate(street):
-        num_house = int(elem[1])
+
+    for i, num in enumerate(street):
+        num_house = int(num)
+
+        if i_zero is not None and num_house != 0:
+            diff_to_zero[i] = str(i - i_zero)
+
         if num_house == 0:
             pre_i_zero = i_zero
-            i_zero = elem[0]
+            i_zero = i
             diff_to_zero[i_zero] = '0'
+
             if pre_i_zero is not None:
                 diff_before_i_zero(
                     pre_i_zero + (i_zero - pre_i_zero) // 2 + 1,
                     diff_to_zero,
                     i_zero
                 )
-            if pre_i_zero is None and elem[0] != 0:
+
+            if pre_i_zero is None and i != 0:
                 diff_before_i_zero(0, diff_to_zero, i_zero)
-        if i_zero is not None and num_house != 0:
-            diff_to_zero[elem[0]] = str(elem[0] - i_zero)
-    with open('output.txt', 'w') as writer:
+
+    with open('../output.txt', 'w') as writer:
         writer.write(' '.join(diff_to_zero))
 
 
-main()
+if __name__ == '__main__':
+    main()
